db2ff556b757fec72c81c47dcde0153105fca3fc
Adil Yergaliyev <adil.yergaliyev@gmail.com>
2025-04-18 20:13:04 +0200

    Creates fresh xcode project.

---
+++ AiraloAssignment/AiraloAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiraloAssignmentApp.swift
+//  AiraloAssignment
+//
+//  Created by Adil Yergaliyev on 18.04.25.
+//
+
+import SwiftUI
+
+@main
+struct AiraloAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AiraloAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AiraloAssignment
+//
+//  Created by Adil Yergaliyev on 18.04.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
